59948ae9494f86bf8979303145e597a9dffb04d4
nndmw <yrzck@protonmail.com>
2021-04-08 22:17:18 +0800

    modify tags

--- config.h
+++ config.h
@@ -35,7 +35,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "\uf120", "\uf738", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
 
 static const Rule rules[] = {
 	/* xprop(1):
